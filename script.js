26b3b285bbeba3ee7dc2e8705b6748438b27fe8e
Felipe <feduarte.dev@gmail.com>
2023-03-28 00:35:38 -0300

    added about me

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ function toggleMode() {
     /*if it doesnt finds class light, use default avatar image*/
   }
 }
+
+const targetDiv = document.getElementById("third")
+const btn = document.getElementById("toggle")
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none"
+  } else {
+    targetDiv.style.display = "block"
+  }
+}
